1bca0096b607336fac54153bba2f8be91377f720
apchalco24 <alexis16tda@hotmail.com>
2021-06-24 00:02:09 -0500

    Axios GET y tablas bootstrapvue

--- src/components/ObtenerTickets.vue
+++ src/components/ObtenerTickets.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        <h1>Ticktes</h1>
+        <b-card>
+            <b-table striped hover :items="tickets"></b-table>
+        </b-card>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+    export default {
+        name: 'ObtenerVuelos',
+        data() {
+            return{
+                tickets: [],
+            }
+        },
+        methods: {
+            async getAllTickets(){
+                let res = await axios.get(`http://localhost:8000/flights/getTickets`)
+                console.log(res.data.oTicketsFlight);
+                this.tickets = await res.data.oTicketsFlight;
+            }
+        },
+        created(){
+            this.getAllTickets();
+        }
+    }
+</script>
--- src/components/ObtenerVuelos.vue
+++ src/components/ObtenerVuelos.vue
@@ -1,14 +1,29 @@
 <template>
     <div>
-        <h1>Vuelos</h1>
+        <b-card>
+            <b-table striped hover :items="vuelos"></b-table>
+        </b-card>
     </div>
 </template>
 
 <script>
-export default {
-  name: 'ObtenerVuelos',
-  props: {
-    msg: String
-  }
-}
+    import axios from 'axios';
+    export default {
+        name: 'ObtenerVuelos',
+        data() {
+            return{
+                vuelos: [],
+            }
+        },
+        methods: {
+            async getAllFlights(){
+                let res = await axios.get(`http://localhost:8000/flights/getFlights`)
+                console.log(res.data.oFlight);
+                this.vuelos = await res.data.oFlight;
+            }
+        },
+        created(){
+            this.getAllFlights();
+        }
+    }
 </script>
